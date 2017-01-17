92cef0ed06a49b95660ad9afabff841b868dae82
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2017-01-17 11:45:12 +0100

    updated mxnet-fcn files to avoid offset-error (https://github.com/dmlc/mxnet/issues/3638)

--- mxnet-fcn/image_segmentaion.py
+++ mxnet-fcn/image_segmentaion.py
@@ -2,12 +2,29 @@
 import numpy as np
 import mxnet as mx
 from PIL import Image
-import matplotlib.pyplot as plt
-import sys
 
-img = sys.argv[1]
-seg = sys.argv[2]
-model_previx = "mxnet-fcn/FCN8s_VGG16"
+def getpallete(num_cls):
+        # this function is to get the colormap for visualizing the segmentation mask
+        n = num_cls
+        pallete = [0]*(n*3)
+        for j in xrange(0,n):
+                lab = j
+                pallete[j*3+0] = 0
+                pallete[j*3+1] = 0
+                pallete[j*3+2] = 0
+                i = 0
+                while (lab > 0):
+                        pallete[j*3+0] |= (((lab >> 0) & 1) << (7-i))
+                        pallete[j*3+1] |= (((lab >> 1) & 1) << (7-i))
+                        pallete[j*3+2] |= (((lab >> 2) & 1) << (7-i))
+                        i = i + 1
+                        lab >>= 3
+         return pallete
+
+pallete = getpallete(256)
+img = "./person_bicycle.jpg"
+seg = img.replace("jpg", "png")
+model_previx = "FCN8s_VGG16"
 epoch = 19
 ctx = mx.gpu(0)
 
@@ -33,31 +50,9 @@ def main():
     exector.forward(is_train=False)
     output = exector.outputs[0]
     out_img = np.uint8(np.squeeze(output.asnumpy().argmax(axis=1)))
-    classed = out_img
-
-    net_root = 'caffe-fcn/fcn-8s'
-    names = dict()
-    all_labels = ["0: Background"] + open(net_root + '/legend.txt').readlines() # legent is from PASCAL-Context_59 (http://www.cs.stanford.edu/~roozbeh/pascal-context/), but first 20 labels are identical to VOC2012 (http://host.robots.ox.ac.uk/pascal/VOC/voc2012/segexamples/index.html), which was used to train the FCN8s_VGG16 used here
-    scores = np.unique(classed)
-    labels = [all_labels[s] for s in scores]
-    num_scores = len(scores)
-
-
-    def rescore(c):
-        """ rescore values from original score values (0-59) to values ranging from
-        0 to num_scores-1 """
-        return np.where(scores == c)[0][0]
-
-    rescore = np.vectorize(rescore)
-    painted = rescore(classed)
-
-    plt.figure(figsize=(10, 10))
-    plt.imshow(painted)
-    formatter = plt.FuncFormatter(lambda val, loc: labels[val])
-    plt.colorbar(ticks=range(0, num_scores), format=formatter)
-    plt.clim(-0.5, num_scores - 0.5)
-
-    plt.savefig(sys.argv[2])
+    out_img = Image.fromarray(out_img)
+    out_img.putpalette(pallete)
+    out_img.save(seg)
 
 if __name__ == "__main__":
     main()
